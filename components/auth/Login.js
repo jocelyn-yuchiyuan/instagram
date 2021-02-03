da34e1779f419b32ae54ea9a6fced861cf0c79ef
jocelynyuan <jocelyn19950627@yahoo.com.tw>
2021-02-03 14:53:12 +0800

    auth

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,53 +1,51 @@
 import React, { Component } from 'react'
 import { View, Button, TextInput } from 'react-native'
+
 import firebase from 'firebase'
 
+export class Login extends Component {
+    constructor(props) {
+        super(props);
 
-export class Login extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      email: '',
-      password:'',
+        this.state = {
+            email: '',
+            password: '',
+        }
 
+        this.onSignUp = this.onSignUp.bind(this)
     }
-     this.onSignUp = this.onSignUp.bind(this)
-  }
 
+    onSignUp() {
+        const { email, password } = this.state;
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then((result) => {
+                console.log(result)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
 
-  onSignUp(){
-    const{ email, password, name } = this.state;
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((result) => {
-        console.log(result)
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
 
-      })
-      .catch((error) => {
-        console.log(error)
-
-      })
-
-  }
-
-  render() {
-    return (
-      <View>
-
-      <TextInput
-          placeholder="email"
-          onChangeText={ (email) => this.setState({ email })}
-      />
-      <TextInput
-          placeholder="password"
-          secureTextEntry = {true}
-          onChangeText={ (password) => this.setState({ password })}
-      />
-      <Button
-        onpress= {()=> this.onSignUp()}
-        title = "Sign in"
-      />
-      </View>
-    )
-  }
+                <Button
+                    onPress={() => this.onSignUp()}
+                    title="Sign In"
+                />
+            </View>
+        )
+    }
 }
+
 export default Login
